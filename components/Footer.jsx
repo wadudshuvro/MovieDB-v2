f6077644c167e8fb5007d1fa20036394b680ade1
wadud.shuvro <wadudshuvro1993@gmail.com>
2024-04-18 21:28:14 +0600

    add

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="py-6 md:py-8 mt-8">
       <div className="container mx-auto">
         <p className="text-center text-sm text-black/30 dark:text-[#EEEEEE]/60">
-          Copyright ©2024 | All rights reserved by Learn with Sumit
+          Copyright ©2024 | All rights reserved by Learn with Sumit hello
         </p>
       </div>
     </footer>
